5114404a0191aa0ef07ebf57681ee6e84cf73fae
Matt Leuer <mattleuer@gmail.com>
2019-10-07 12:44:42 -0500

    Initial commit.

---
+++ DyeListGeneratorUI/ViewController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using AppKit;
+using Foundation;
+
+namespace DyeListGeneratorUI
+{
+    public partial class ViewController : NSViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Do any additional setup after loading the view.
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get { return base.RepresentedObject; }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+
+        partial void CustomerOrdersFilePickerButtonClicked(Foundation.NSObject sender)
+        {
+            NSOpenPanel openPanel = new NSOpenPanel();
+            openPanel.AllowsMultipleSelection = false;
+            openPanel.CanChooseDirectories = false;
+            openPanel.CanCreateDirectories = false;
+            openPanel.CanChooseFiles = true;
+
+            long userInput = openPanel.RunModal();
+            if(userInput == (long)NSModalResponse.OK)
+            {
+                var file = openPanel.Filenames[0];
+            }
+        }
+    }
+}
---
+++ DyeListGeneratorUI/ViewController.designer.cs
@@ -0,0 +1,28 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace DyeListGeneratorUI
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Action ("CustomerOrdersFilePickerButtonClicked:")]
+		partial void CustomerOrdersFilePickerButtonClicked (Foundation.NSObject sender);
+
+		[Action ("GenerateDyeListButtonClicked:")]
+		partial void GenerateDyeListButtonClicked (Foundation.NSObject sender);
+
+		[Action ("MasterDyeListFilePickerButtonClicked:")]
+		partial void MasterDyeListFilePickerButtonClicked (Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
